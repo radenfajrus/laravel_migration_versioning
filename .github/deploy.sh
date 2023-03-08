b916186b93ba33548e1813b4c057fc7fd9650a65
Raden Fajrus <radenfajrus@gmail.com>
2023-03-08 16:06:47 +0700

    v1.0

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -5,10 +5,6 @@ DEPLOYMENT_COMMIT=`[ -z $1 ] && echo 'prod' || echo $1`
 set -e
 echo "Deployment started ..."
 
-git stash
-git fetch --all
-git checkout $DEPLOYMENT_COMMIT
-
 #php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 #php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 #php composer-setup.php
